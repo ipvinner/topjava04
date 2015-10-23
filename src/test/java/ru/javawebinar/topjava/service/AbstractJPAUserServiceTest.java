584e6d2979c8e06e67f3f576994df53179d50c9e
Ivan Vynogradov <vaniavinogradov@rambler.ru>
2015-10-23 10:34:25 +0300

    HW06

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJPAUserServiceTest.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+
+/**
+ * Created by vinner on 21.10.2015.
+ */
+abstract public class AbstractJPAUserServiceTest extends AbstractUserServiceTest {
+
+}
